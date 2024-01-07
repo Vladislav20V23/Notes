e0dd4ae3e78d31a48ddb57d83d519280bea4e9c5
Vladislav20V23 <151963265+Vladislav20V23@users.noreply.github.com>
2024-01-07 15:46:25 +0200

    new

--- src/main/java/com/notes/detail/spring/rest/configuration/Config.java
+++ src/main/java/com/notes/detail/spring/rest/configuration/Config.java
@@ -1,2 +1,49 @@
-package com.notes.detail.spring.rest.configuration;public class Config {
+package com.notes.detail.spring.rest.configuration;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+
+@Configuration
+@ComponentScan(basePackages = "com.notes.detail.spring.rest")
+@EnableWebMvc
+public class Config {
+
+    @Bean
+    public DataSource dataSource(){
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+
+        try {
+            dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?useSSL=false&amp;serverTimezone=UTC");
+            dataSource.setUser("bestuser");
+            dataSource.setPassword("bestuser");
+
+        } catch (PropertyVetoException e) {
+            throw new RuntimeException(e);
+        }
+
+        return dataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactoryBean(){
+
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("com.notes.detail.spring.rest.entity");
+
+
+
+        return null;
+    }
+
+
 }
